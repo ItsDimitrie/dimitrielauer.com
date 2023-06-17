30493aaf914b6136c8a0742948fad9cf3afc211c
ItsDimitrie <laue0002@HZ.nl>
2023-06-17 11:16:06 +0200

    Website conversie naar Laravel website.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('content')
+  <section class="jumbotron" id="aboutmeheader">
+    <div class="container">
+      <h2> About Me </h2>
+    </div>
+  </section>
+  <section class="container">
+    <div class="col-sm-4">
+      <p> Hi! My name is Dimitrie Lauer and I am a freelance Camera-Operator and Learning Software Developer.</p>
+
+      <p>I have started my camera journey when I was 15 years old. After 4 years of education on the "Curio College" I
+        started working at EMG Nederland the biggest broadcast facilitatior in the Netherlands.</p>
+
+      <p>Now after 3 years of working at EMG I have decided to become a Freelancer! If you are interested in my work
+        check out the "My Work" page on this website!</p>
+
+      <p> In 2022 I applied to the HZ University of Applied Science to study HBO-ICT witch I am studying right now!
+        This web page is a starting project of mine in my first year.</p>
+
+      <p> When I started working as a freelance camera-operator I also founded the company "DL Video-Registraties.
+        This is the name I use when working as a camera-operator so you will
+        see that coming around very often!
+      </p>
+      <img src="img/IMG_8994.gif" id="aboutmefoto">
+    </div>
+  </section>
+
+@endsection
---
+++ resources/views/education.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+
+@section('content')
+
+  <section class="jumbotron" id="aboutmeheader">
+    <div class="container">
+      <h2> Education </h2>
+    </div>
+  </section>
+  <section class="container" id="BOZ">
+    <div class="col-sm-6">
+      <h3 class="font-weight-bold">Curio College Bergen op Zoom</h3>
+      <img class="portrait"
+        src="https://www.curio.nl/media/xavjd2ww/locatie-curio-dr-ir-van-veenweg-8-2.jpg?anchor=center&mode=crop&width=1280&rnd=132494817026330000&quality=70">
+      <ul style="margin-top: 50px;">
+        <li>AV-Specialist</li>
+        <li>Dutch MBO Level 4</li>
+        <li> Broadcast and Journalism programs.</li>
+      </ul>
+    </div>
+    <div class="col-sm-6">
+      <h3 class="font-weight-bold">HZ University of Applied Science</h3>
+      <img class="portrait"
+        src="https://storage.pubble.nl/67c86d35/content/2018/9/668b41d2-be80-4328-9bac-e34ab3159483_thumb840.jpg">
+      <ul style="margin-top: 50px;">
+        <li>IT & Software Develpoment.</li>
+        <li>HBO Level</li>
+      </ul>
+    </div>
+  </section>
+
+@endsection
